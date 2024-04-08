0a72a29070e639eeb2fb8a088cbdb4e25fb949fc
Falkono <yoda0108@gmail.com>
2024-04-08 16:11:22 +0200

    parent test methods heads

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -4,5 +4,20 @@ from parentnode import ParentNode
 
 #TODO need some juice tests
 class TestParentNode(unittest.TestCase):
-    pass
-        
+    def test_only_leafnodes():
+        pass
+
+    def test_leafnodes_with_one_nested_parent():
+        pass
+
+    def test_leafnodes_with_two_parents():
+        pass
+
+    def test_leafnodes_with_a_parent_with_a_parent():
+        pass 
+
+    def test_parent_without_tag():
+        pass
+
+    def test_parent_without_childern():
+        pass
