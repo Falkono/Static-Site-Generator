642a3b3c97014646261c0ab674b08e93466cec0e
Falkono <yoda0108@gmail.com>
2024-04-08 14:25:30 +0200

    added parentnode logic

--- src/parentnode.py
+++ src/parentnode.py
@@ -1,7 +1,9 @@
 from htmlnode import HTMLNode
 
+#TODO need to be tested
+
 class ParentNode(HTMLNode):
-    def __init__(self, tag=None, children=[], props={}):
+    def __init__(self, tag=None, children:[]=[], props={}):
         super().__init__(tag, children=children, props=props)
 
     def to_html(self):
@@ -10,4 +12,9 @@ class ParentNode(HTMLNode):
         if self.children == None:
             raise ValueError("No Children provided")
         
+        output_string = ""
+        for child in self.children:
+            output_string += child.to_html()
+        return f"<{self.tag}>{output_string}</{self.tag}>"
+
     
