b144ff3159be606aa3e500e7ffce50ab432ee71f
Falkono <yoda0108@gmail.com>
2024-04-08 14:25:48 +0200

    add the file to test the parentnodes logic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 from textnode import TextNode
 from leafnode import LeafNode
+from parentnode import ParentNode
 
 def main():
-    textNode = TextNode("This is a text", "bold", "https://www.boot.dev")
+    node = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+            ParentNode("p",[LeafNode("b","Bold Tex"), LeafNode("i","italic text"),ParentNode("p",[LeafNode("b","Bold Tex"), LeafNode("i","italic text")])])
+        ],
+    )
+
+    print(node.to_html())
+
+   
     
 main()
---
+++ src/test_parentnode.py
@@ -0,0 +1,8 @@
+import unittest
+
+from parentnode import ParentNode
+
+#TODO need some juice tests
+class TestParentNode(unittest.TestCase):
+    pass
+        
